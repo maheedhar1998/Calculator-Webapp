2efab9980de775b08c954b8ea0f1d767f11e80c2
maheedhar1998 <maheedhar1998@hotmail.com>
2020-11-03 12:46:05 -0500

    Almost Working
    Implemented basic calculator UI and basic arithmetic funtionality
    Integrated Firebase to contain a list of the last 10 calculations on the app

--- Calculator-Webapp/src/app/app.module.ts
+++ Calculator-Webapp/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import firebase from 'firebase';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,8 +17,25 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    BackgroundMode,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDTPCzCCdTe1dJbY_N2NM97L4ucev1hPS4",
+      authDomain: "calculator-webapp-dbeef.firebaseapp.com",
+      databaseURL: "https://calculator-webapp-dbeef.firebaseio.com",
+      projectId: "calculator-webapp-dbeef",
+      storageBucket: "calculator-webapp-dbeef.appspot.com",
+      messagingSenderId: "42496363585",
+      appId: "1:42496363585:web:7b23c11943880f46f80ac0",
+      measurementId: "G-8XT7P2763M"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
+}
--- Calculator-Webapp/src/app/home/home.page.ts
+++ Calculator-Webapp/src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import * as math from '../../../math.min.js';
+import firebase from 'firebase';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,107 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  private recent: string[];
+  private calc: string;
+  private sol: string;
+  private history: string[];
+  private checkChange: any;
+  constructor(public bgMode: BackgroundMode) {
+    this.recent = [];
+    firebase.database().ref('last10/').once("value", (snap)=> {
+      snap.forEach((shot) => {
+        this.recent.push(shot.val());
+      });
+    });
+    this.checkChange = interval(500).subscribe(() => {
+      firebase.database().ref('last10/').once("child_changed", (snap)=> {
+        //this.recent = [];
+        console.log(snap.val());
+        if(snap.val() != this.recent[0]) {
+          if(this.recent.unshift(snap.val())>10) {
+            this.recent.pop();
+          }
+        }
+      });
+    });
+    this.calc = "";
+    this.sol = "";
+    this.history = [];
+  }
+  press0() {
+    this.calc += "0";
+  }
+  press1() {
+    this.calc += "1";
+  }
+  press2() {
+    this.calc += "2";
+  }
+  press3() {
+    this.calc += "3";
+  }
+  press4() {
+    this.calc += "4";
+  }
+  press5() {
+    this.calc += "5";
+  }
+  press6() {
+    this.calc += "6";
+  }
+  press7() {
+    this.calc += "7";
+  }
+  press8() {
+    this.calc += "8";
+  }
+  press9() {
+    this.calc += "9";
+  }
+  pressDec() {
+    this.calc += ".";
+  }
+  pressEquals() {
+    try {
+      if(this.calc != "") {
+        this.sol = math.evaluate(this.calc);
+        let totCal: string = this.calc+" = "+this.sol;
+        this.recent = [];
+        firebase.database().ref('last10/').once("value", (snap)=> {
+          snap.forEach((shot) => {
+            this.recent.push(shot.val());
+          });
+          if(this.recent.unshift(totCal)>10) {
+            this.recent.pop();
+          }
+          console.log(this.recent);
+          var updates = {};
+          updates['last10'] = this.recent;
+          firebase.database().ref().update(updates);
+          this.history.push(totCal);
+          this.calc = "";
+          // console.log(this.history);
+        });
+      } else {
+        alert("Empty Expression");
+      }
+    }
+    catch(e) {
+      alert(e);
+      this.history.push(this.calc+" = Error");
+      this.calc = "";
+    }
+  }
+  pressDivide() {
+    this.calc += " / ";
+  }
+  pressMultiply() {
+    this.calc += " * ";
+  }
+  pressSubtract() {
+    this.calc += " - ";
+  }
+  pressAdd() {
+    this.calc += " + ";
+  }
 }
